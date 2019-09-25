9dfca422802169510e0d1f0bf2d8a1c56600487b
tha <tha@easv.dk>
2019-09-25 13:43:42 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Declararation
+
+        int inputTemp = 23;
+
+
+        
+
+
+        System.out.println("");
+
+
+
+
+    }
+}
