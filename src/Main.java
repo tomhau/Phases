5c6435b87597e4efa6fc543dbca19bdfcc459230
tha <tha@easv.dk>
2020-09-17 13:11:24 +0200

    Corrected errors

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
 
-
+// import
 public class Main {
 
     public static void main(String[] args) {
 
-        // (1) Declararation
+        // (1) Declaration
 
+        // Scanner object
+        double bookPrice;
+
+        double totalPrice=0.0;
+        // prompt user
+        // bookPrice = ...
         // (2) Getting input
 
-        // (3) Cmputation(s)
-
+        // (3) Computation(s)
 
         // (4) Presenting output
+
+
 
 
 
